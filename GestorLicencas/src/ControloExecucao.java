5fba216b098404b6e317883bffd5943f95747ccc
João Oliveira <joaop.oliveira@ua.pt>
2023-11-19 22:25:06 +0000

    adiciona lib para controlo de execucao da app

---
+++ GestorLicencas/src/ControloExecucao.java
@@ -0,0 +1,19 @@
+public class ControloExecucao {
+
+    void init(String nomeApp, String versao) {
+
+    }
+
+    boolean isRegistered() {
+        return false;
+    }
+
+    boolean startRegistration() {
+        return false;
+    }
+
+    void showLicenseInfo() {
+
+    }
+
+}
