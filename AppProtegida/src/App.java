1b8af1cdfabf4474d432b65133054ad062060d98
João Oliveira <joaop.oliveira@ua.pt>
2023-11-25 19:36:03 +0000

    adiciona cli para info necessaria para pedido de licença

--- AppProtegida/src/App.java
+++ AppProtegida/src/App.java
@@ -5,6 +5,12 @@ public class App {
 
         if (!ce.isRegistered()) {
             System.out.println("Uso não autorizado da aplicação!");
+
+            if (ce.startRegistration()) {
+                System.out.println("Processo de criação de pedido de licença executado com sucesso!");
+            } else {
+                System.out.println("Erro ao tentar criar pedido de licença!");
+            }
             return;
         }
 
--- ControloExecucao/src/ControloExecucao.java
+++ ControloExecucao/src/ControloExecucao.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class ControloExecucao {
 
     public ControloExecucao(String appName, String version) {
@@ -9,7 +11,65 @@ public class ControloExecucao {
     }
 
     boolean startRegistration() {
-        return false;
+        //User Data - name, email, nic, cc certificate
+        String userName;
+        String userEmail;
+        String userNIC;
+        //System Data - nr&type CPU, mac addresses
+        int cpus;
+        String cpuType;
+        String macAddresses;
+        //App Data - name, version, hash
+        String appName;
+        String version;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Processo de criação de pedido de licença");
+        do {
+            System.out.println("Enter your name:");
+            userName = sc.nextLine().trim();
+        } while (userName.isEmpty());
+
+        do {
+            System.out.println("Enter your email:");
+            userEmail = sc.nextLine();
+        } while (userEmail.isEmpty());
+
+        do {
+            System.out.println("Enter your nic:");
+            userNIC = sc.nextLine();
+        } while (userNIC.isEmpty());
+
+        do {
+            System.out.println("Enter the number of CPUs:");
+            cpus = sc.nextInt();
+            sc.nextLine();
+        } while (cpus <= 0);
+
+        do {
+            System.out.println("Enter the type of CPUs:");
+            cpuType = sc.nextLine();
+        } while (cpuType.isEmpty());
+
+        do {
+            System.out.println("Enter the MAC Addresses:");
+            macAddresses = sc.nextLine();
+        } while (macAddresses.isEmpty());
+
+        do {
+            System.out.println("Enter the app's name:");
+            appName = sc.nextLine();
+        } while (appName.isEmpty());
+
+        do {
+            System.out.println("Enter the app's current version:");
+            version = sc.nextLine();
+        } while (version.isEmpty());
+
+        sc.close();
+
+        return true;
     }
 
     void showLicenseInfo() {
